6183bfb6ba1becaff0576899500ddd0cea87bc0e
PattyRik <myersp94@gmail.com>
2026-01-12 11:59:55 -0800

    Potentiometer added for tuning delay
    connect 5k or 10k pot between 5v and ground, connect the middle pin to A0, confirm it works with the serial monitor.

--- SigJam/SigJam.ino
+++ SigJam/SigJam.ino
@@ -1,4 +1,4 @@
-#include <ELECHOUSE_CC1101.h
+#include <ELECHOUSE_CC1101.h>
 
 #define size 1
 byte TX_buffer[size] = {0};
@@ -12,8 +12,11 @@ void setup()
 
 void loop() 
 {
+  int potReading = analogRead(A0);
+  float delayTimer = potReading * (5.0 / 1023);
   TX_buffer[0] = 5;
   ELECHOUSE_cc1101.SendData(TX_buffer, size);
   Serial.println("Sent: 5");
-  delay(1000); //delay 1 second, change to analog read
+  Serial.println(delayTimer);
+  delay(potReading); //delay 1 second, change to analog read
 }
